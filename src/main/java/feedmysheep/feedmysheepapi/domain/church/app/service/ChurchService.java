39310de2cc37313a110fc841f19b4d4390170bda
santino <santinomin0381@gmail.com>
2023-11-14 16:57:35 +0900

    DB에 접근하는 부분 아직 수정 필요.

--- src/main/java/feedmysheep/feedmysheepapi/domain/church/app/service/ChurchService.java
+++ src/main/java/feedmysheep/feedmysheepapi/domain/church/app/service/ChurchService.java
@@ -1,14 +1,19 @@
 package feedmysheep.feedmysheepapi.domain.church.app.service;
 
+import feedmysheep.feedmysheepapi.domain.church.app.dto.ChurchReqDto;
 import feedmysheep.feedmysheepapi.domain.church.app.dto.ChurchResDto;
+import feedmysheep.feedmysheepapi.domain.church.app.dto.ChurchResDto.getChurchList;
 import feedmysheep.feedmysheepapi.domain.church.app.repository.ChurchRepository;
 import feedmysheep.feedmysheepapi.domain.member.app.repository.MemberRepository;
 import feedmysheep.feedmysheepapi.global.utils.jwt.CustomUserDetails;
 import feedmysheep.feedmysheepapi.global.utils.response.error.CustomException;
 import feedmysheep.feedmysheepapi.global.utils.response.error.ErrorMessage;
+import feedmysheep.feedmysheepapi.models.ChurchEntity;
+import java.util.ArrayList;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Service
 public class ChurchService {
@@ -39,4 +44,24 @@ public class ChurchService {
     // 2. 교회 리스트 반환
     return this.churchRepository.getAllValidChurchList();
   }
+
+  public List<ChurchResDto.getChurchList> registerChurch(ChurchResDto.getChurchList body){
+    // ChurchResDto.getChurchList 객체에서 교회 정보들을 추출.
+    String churchName = body.getChurchName();
+    String churchLocation = body.getChurchLocation();
+
+//      새로운 교회 정보를 생성하고, DB에 저장하기.
+//    ChurchEntity newChurch = new ChurchEntity();
+//    newChurch.setChurchName(churchName);
+//    newChurch.setChurchLocation(churchLocation);
+//    newChurch.setValid(true); // 예시로 유효성을 true로 설정
+
+    // 새로운 교회 정보를 데이터베이스에 저장
+//    churchRepository.save(newChurch);
+
+    List<ChurchResDto.getChurchList> churchList = churchRepository.getAllValidChurchList();
+
+    return churchList;
+  }
+
 }
